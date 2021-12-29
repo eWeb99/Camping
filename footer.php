c3c29294012699d42c59f77cc5f19aa04d58bf92
eWeb089 <78365886+eWeb089@users.noreply.github.com>
2021-12-29 21:42:39 +0100

    Summary
    Hey

---
+++ footer.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+ <section id="footer">
+    <div class="footer-all">
+    <div class="footer">
+        <div class="footer-links">
+            <ul class="adresse">
+            <li><i class="fas fa-map-marker-alt text-primary"></i></li>
+                <span>89331 Burgau<br>Koordinaten: 48.406234, 10.406206</span>
+            </ul>
+            <ul class="email">
+            <li>
+            <i class="fas fa-envelope text-primary"></i>
+                <li>
+                <span>dn.business@web.de</span>
+            </li>
+            </ul>
+            <ul class="tel">
+                <li>
+                <i class="fas fa-phone text-primary"></i>
+                </li>
+                <li>
+                <span>01566737098</span>
+            </li>
+            </ul>
+        </div>
+
+        <div class="footer-rechts">
+                <div class="campingplatz">
+                    <ul>
+                        <p class = "p-camping"><strong>Campingplatz</strong><p>
+
+                        <li class = "footer-rechts-li">
+                        <a href="include/corona">Corona</a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="umgebung">
+                <ul>
+                    <p class="p-umgebung"><strong>Umgebung</strong></p>
+                        <li class = "footer-rechts-li">
+                        <a href="include/campingplatz">Landkreise</a>
+                        </li>
+                        
+
+                    </ul>
+                </div>
+                <div class="wissenwertes">
+                <ul>
+                    <p class="p-wissenwertes"><strong>Wissenwertes</strong></p>
+
+
+
+                        <li class = "footer-rechts-li">
+                        <a href="include/lage">Lage & Anfahrt</a>
+                        </li>
+
+                        <li class = "footer-rechts-li">
+                        <a href="include/platzregeln">Platzregeln</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+        </div>
+
+    </div>
+            <ul class="footer-impressum"><p>&copy; 2022 camping-burgau.com</p>
+                <li>
+                    <a href="include/impressum">Impressum</a>
+                </li>
+                <li>
+                <a href="include/impressum">AGBs</a>
+                    
+                </li>
+
+                <li>
+                <a href="include/impressum">Datenschutz</a>
+                    
+                </li>
+
+                
+            </ul>
+        <div class="footer-erfunden">
+           <p><i class="fas fa-exclamation text-primary1"></i>Diese Webseite ist aus reiner Freude am Schreiben von Webseiten, frei erfunden worden. Sie hat kein wirklichen Nutzen.</p>
+        </div>
+        </div>
+ </section>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- <script>
+    alert("DAS IST NUR EIN VORRÜBERGEHNDER TEST")
+</script> -->
+<?php include('include/head.php')?>
+
+<?php include('include/header.php')?>
+
+<?php include('include/welcome.php')?>
+
+<?php include('include/stellplatz.php')?>
+
+<?php include('include/referenzen.php')?>
+
+<?php include('include/footer.php')?>
